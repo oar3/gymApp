3105cc4e49873530ba64629b00fbfeaa544e0ced
otisrobson <otis@klever.co.uk>
2025-04-29 23:58:43 +0100

    user-created styling changes.

--- resources/views/mail/user-created.blade.php
+++ resources/views/mail/user-created.blade.php
@@ -94,27 +94,8 @@
 <body>
 <div class="container">
     <div class="header">
-        <!-- Dumbbell icon embedded inline -->
-        <svg class="logo" fill="#FFFFFF" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             viewBox="0 0 485.535 485.535" xml:space="preserve">
-            <g>
-                <g id="_x35__13_">
-                    <g>
-                        <path d="M55.465,123.228c-15.547,0-28.159,12.608-28.159,28.161v56.673C11.653,211.908,0,225.928,0,242.765
-                            c0,16.842,11.652,30.861,27.306,34.707v56.666c0,15.555,12.612,28.16,28.159,28.16c15.546,0,28.16-12.605,28.16-28.16V151.389
-                            C83.625,135.837,71.011,123.228,55.465,123.228z"/>
-                        <path d="M334.498,65.278c-23.092,0-41.811,18.719-41.811,41.812v93.864h-12.801h-60.585h-19.625l-6.827-0.163V107.09
-                            c0-23.092-18.72-41.812-41.813-41.812c-23.091,0-41.812,18.719-41.812,41.812v271.355c0,23.093,18.721,41.812,41.812,41.812
-                            c23.094,0,41.813-18.719,41.813-41.812v-93.653c0,0,4.501-0.211,6.827-0.211h19.625h60.585h12.801v93.864
-                            c0,23.093,18.719,41.812,41.811,41.812c23.094,0,41.812-18.719,41.812-41.812V107.089
-                            C376.311,83.998,357.592,65.278,334.498,65.278z"/>
-                        <path d="M458.229,208.062v-56.673c0-15.552-12.613-28.161-28.158-28.161c-15.547,0-28.16,12.608-28.16,28.161v182.749
-                            c0,15.555,12.613,28.16,28.16,28.16c15.545,0,28.158-12.605,28.158-28.16v-56.666c15.654-3.846,27.307-17.865,27.307-34.707
-                            C485.535,225.927,473.883,211.908,458.229,208.062z"/>
-                    </g>
-                </g>
-            </g>
-        </svg>
+        <!-- Using $message->embed for the logo image -->
+        <img src="{{ $message->embed(public_path('resources/images/dumbbell.svg')) }}" alt="Gym App Logo" class="logo">
         <h1>Welcome to Gym App</h1>
     </div>
 
@@ -133,9 +114,8 @@
                 <td>
                     <div class="feature-box">
                         <div class="icon-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#FFFFFF">
-                                <path d="M10 2C5.59 2 2 5.59 2 10s3.59 8 8 8 8-3.59 8-8-3.59-8-8-8zm0 14c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm-.5-9v4.5H13l-3.5-4.5z"/>
-                            </svg>
+                            <!-- Using $message->embed for feature icons -->
+                            <img src="{{ $message->embed(public_path('images/track-icon.png')) }}" alt="Track Icon" width="20" height="20">
                         </div>
                         <h3 class="feature-title">Track Your Progress</h3>
                         <p>Log workouts and monitor your strength improvements over time.</p>
@@ -146,9 +126,7 @@
                 <td>
                     <div class="feature-box">
                         <div class="icon-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#FFFFFF">
-                                <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm0 10a4 4 0 110-8 4 4 0 010 8zm6-10a1 1 0 100 2 1 1 0 000-2zM5 14a1 1 0 100 2 1 1 0 000-2z"/>
-                            </svg>
+                            <img src="{{ $message->embed(public_path('images/explore-icon.png')) }}" alt="Explore Icon" width="20" height="20">
                         </div>
                         <h3 class="feature-title">Explore Exercises</h3>
                         <p>Discover new exercises or create custom ones specific to your routine.</p>
@@ -159,9 +137,7 @@
                 <td>
                     <div class="feature-box">
                         <div class="icon-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="#FFFFFF">
-                                <path d="M10 2a8 8 0 100 16 8 8 0 000-16zm0 14a6 6 0 110-12 6 6 0 010 12zm1-6.41V4h-2v4.59l3 3 1.41-1.41-2.41-2.42z"/>
-                            </svg>
+                            <img src="{{ $message->embed(public_path('images/consistency-icon.png')) }}" alt="Consistency Icon" width="20" height="20">
                         </div>
                         <h3 class="feature-title">Stay Consistent</h3>
                         <p>Build the habit by regularly logging your workouts.</p>
@@ -183,6 +159,8 @@
     </div>
 
     <div class="footer">
+        <!-- Using $message->embed for footer logo -->
+        <img src="{{ $message->embed(public_path('images/small-logo.png')) }}" alt="Small Logo" width="30" height="30">
         <p>&copy; {{ date('Y') }} Gym App. All rights reserved.</p>
         <p>
             <a href="{{ url('/terms') }}" style="color: #666; text-decoration: none; margin: 0 5px;">Terms</a> |
