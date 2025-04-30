c09b34321e6ede350673a5644f34916d47a5c313
otisrobson <otis@klever.co.uk>
2025-04-30 11:09:04 +0100

    Mail on register testing

--- resources/views/mail/user-created.blade.php
+++ resources/views/mail/user-created.blade.php
@@ -95,7 +95,7 @@
 <div class="container">
     <div class="header">
         <!-- Using $message->embed for the logo image -->
-{{--        <img src="{{ $message->embed(public_path('build/assets/dumbbell-DnflnhPi.svg')) }}" alt="Gym App Logo" class="logo">--}}
+        <img src="{{$message->embed(public_path() . '/images/dumbbell.svg'}}" alt="Gym App Logo" class="logo">
         <h1>Welcome to Gym App</h1>
     </div>
 
